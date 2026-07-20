46ddaf1f397034f36e0ac65cdfea88d8a88ae4f6
Handerson Diniz <scoutmark10@gmail.com>
2026-07-20 13:24:30 -0700

    Atualizar o main.js

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ if (!userName && !isIndexPage) {
 const anoAtual = new Date().getFullYear();
 const footer = document.querySelector(".footer");
 if (footer) {
-  footer.innerHTML = `<p>&copy; ${anoAtual} Handerson Dev. Todos os direitos reservados.</p>`;
+  footer.innerHTML = `<p>&copy; ${anoAtual} Zyron Labs💜. Todos os direitos reservados.</p>`;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
